8f0defdf26fffda219ff82066d077527376a0348
Dominik Mikiewicz <dominik@cartomatic.pl>
2015-06-19 15:01:13 +0200

    [jasmine] - Make the test runner 100% separate

--- Jasmine/app/controller/Root.js
+++ Jasmine/app/controller/Root.js
@@ -9,7 +9,8 @@
         extend: 'Ext.app.Controller',
 
         requires: [
-            'Jasmine.view.main.Main'
+            'Jasmine.view.main.Main',
+            'Jasmine.util.TestRunner'
         ],
 
         onLaunch: function () {
@@ -17,13 +18,48 @@
             console.warn('[ROOT CONTROLLER] - on launch...');
             //</debug>
 
-            //check if should handle the normal startup or kick in with the tests mode
-            var hash = window.location.hash.replace('#', ''),
-                query = Ext.Object.fromQueryString(window.location.search);
+            //get the query params, so can dynamically adjust where the tests will be executed
+            //this may be handy if the tests need ui for example
+            var query = Ext.Object.fromQueryString(window.location.search),
+                testRunnerCfg;
 
-            //in order to determine if testing should be initialised, check for the hash and some query string params presence - query params are added by jasmine
-            if(hash === 'selftest' || query.hasOwnProperty('spec') || query.hasOwnProperty('cath') || query.hasOwnProperty('throwFailures')){
-                this.doSelfTest();
+            //check if should handle the normal startup or kick in with the tests mode
+            if(Jasmine.util.TestRunner.isInTestMode()){
+
+                //by default the test runner creates its own viewport that is displayed instead of the app
+                //it is possible to customise this and display the jasmine output on the side or actually in any container
+
+                //the default will run test runner with the standard output generated instead of the app
+                testRunnerCfg = {};
+
+                //display tests on the side
+                if(query.hasOwnProperty('testsonside') && query['testsonside']){
+                    this.viewport = Ext.create('Ext.container.Viewport', {
+                        layout: 'border',
+                        items: [
+                            {
+                                xtype: 'panel',
+                                region: 'center',
+                                layout: 'fit',
+                                items: [
+                                    Ext.create('Jasmine.view.main.Main')
+                                ]
+                            },
+                            {
+                                xtype: 'panel',
+                                region: 'east',
+                                width: 300,
+                                html: '<div id="jasmine_output" style="width:100%; height:100%;"></div>',
+                                overflowY: true,
+                                split: true
+                            }
+                        ]
+                    });
+
+                    testRunnerCfg.outputContainer = 'jasmine_output';
+                }
+
+                Ext.create('Jasmine.util.TestRunner', testRunnerCfg);
             }
             else {
                 //normal app startup
@@ -35,28 +71,6 @@
                     ]
                 });
             }
-        },
-
-        /**
-         * Starts the app in the self test mode
-         */
-        doSelfTest: function(){
-            //<debug>
-            console.warn('[ROOT CONTROLLER] - starting self test mode...');
-            //</debug>
-
-            //Note:
-            //by default the dynamic loading works in the dev mode. in order to also make it work in the production, one needs to make sure
-            //the actual app js stuff is also copied into the app's build dir
-
-            //load the test runner. it will handle all the test related stuff
-            Ext.require(
-                'Jasmine.test.Runner',
-                function(){
-                    Ext.create('Jasmine.test.Runner');
-                },
-                this
-            );
         }
 
     });
